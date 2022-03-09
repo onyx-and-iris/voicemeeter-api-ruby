4deac75b3ba033b6d80095a46f8505a72d1f0a1e
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-03-09 19:51:36 +0000

    Update runvm.rb
    add bits = get_arch in runvm

--- lib/runvm.rb
+++ lib/runvm.rb
@@ -8,9 +8,10 @@ module RunVM
     """
     def start(kind)
         raise VMRemoteErrors.new("Unknown Voicemeeter Kind.") unless $kinds_all.include? kind
+        bits = get_arch
         
         enums = $kinds_all.map.with_index do |val, i|
-            get_arch == 64 && val == "potato" ? [val, i+4] : [val, i+1]
+            bits == 64 && val == "potato" ? [val, i+4] : [val, i+1]
         end
         exes = enums.to_h { |k, v| [k, v.to_i] }
 
