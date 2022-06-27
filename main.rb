e42da31a5ea12c0e7fb9cb4ea6eaeead2a17898a
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-06-27 17:50:41 +0100

    upd main to match readme

--- main.rb
+++ main.rb
@@ -5,15 +5,15 @@ kind_id = 'banana'
 # start Voicemeeter GUI
 Voicemeeter.start(kind_id)
 
-vmr = Voicemeeter.remote(kind_id)
+vm = Voicemeeter.remote(kind_id)
 
-# vmr.run accepts a block
-vmr.run do
+# vm.run accepts a block
+vm.run do
     # mute the leftmost strip
-    vmr.strip[0].mute = true
-    puts vmr.strip[0].mute
+    vm.strip[0].mute = true
+    puts vm.strip[0].mute
 
     # disable eq for second from left bus
-    vmr.bus[1].eq = false
-    puts vmr.bus[1].eq
+    vm.bus[1].eq = false
+    puts vm.bus[1].eq
 end
